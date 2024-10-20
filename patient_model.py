3191f89fbd5a6bb8d1490e00bc8d82c432e57344
khalid5555 <khalid.assuity@yahoo.com>
2024-10-20 16:45:09 +0300

    Changed the comments in the PatientModel class in

--- patient_model.py
+++ patient_model.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel, Field
 
 
 class PatientModel(BaseModel):
-    # id: Optional[int] = None  # يمكن أن يكون هذا الحقل خاليًا
+    id: Optional[int] = None  # يمكن أن يكون هذا الحقل خاليًا
     name: str
     date: str  # يمكنك استخدام datetime بدلاً من string إذا كنت ترغب في ذلك
     mobile: str
